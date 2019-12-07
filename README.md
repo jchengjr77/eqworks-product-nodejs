26e14d3e2e7d38429ae31dfe25eef869ca30ac0a
jchengjr77 <jcheng3@andrew.cmu.edu>
2019-12-07 14:17:27 -0500

    Implemented a simple token bucket API rate limiter.

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-Work Sample for Product Aspect, Node.js Variant
----
+## Work Sample for Product Aspect, Node.js Variant
 
 [What is this for?](https://github.com/EQWorks/work-samples#what-is-this)
 
@@ -30,3 +29,24 @@ The following are the recommended options, but you're free to use any means to g
 ### Notes on working through the problems
 
 Make sure any additional Node.js level dependencies are properly added in `package.json`. We encourage a healthy mixture of your own implementations, and good choices of existing open-source libraries/tools. We will comment in the problems to indicate which ones cannot be solved purely through an off-the-shelf solution.
+
+---
+
+#### API rate limiting notes
+
+Token bucket algorithm is implemented for rate limiting.
+
+- Bucket object can be configured for different rates (not sure what kind of volume this API will handle)
+- Refill bucket at set intervals, with a set amount (all defined by the bucket object).
+- If no tokens left, request is met with an error message.
+
+PROS: simple, easy to maintain, works like a charm, somewhat scalable.
+CONS: No memory of overflow request (the user has to re-request), limit is strict (no dynamic adjustments).
+
+IMPROVEMENTS:
+
+- Implement a queueing system for the requests that were denied. Once there are more tokens in the bucket, then
+  the queued requests can be slowly released.
+- Create dynamic rate limits, such as allowing short surges in requests to pass. This can be done by monitoring
+  an averaged request rate instead of a strict number of tokens. This can lead to a less halting experience for
+  requesters, while still maintaining a limited API request rate.
--- index.js
+++ index.js
@@ -1,54 +1,124 @@
-const express = require('express')
-const pg = require('pg')
+const express = require("express");
+const pg = require("pg");
 
-const app = express()
+const app = express();
 // configs come from standard PostgreSQL env vars
 // https://www.postgresql.org/docs/9.6/static/libpq-envars.html
-const pool = new pg.Pool()
+const pool = new pg.Pool();
 
-const queryHandler = (req, res, next) => {
-  pool.query(req.sqlQuery).then((r) => {
-    return res.json(r.rows || [])
-  }).catch(next)
+// For timing the bucket refills
+const date = new Date();
+
+/**
+ * * Token bucket API rate limiting (simple)
+ * Create a new bucket object
+ */
+var bucket = {
+  capacity: 100 /** Max number of tokens in bucket */,
+  amount: 0 /** Current number of tokens in bucket */,
+  interval: 10000 /** Number of ms between token refills */,
+  last_fill: date.getTime() /** Time of last bucket refill */,
+  refill_amt: 10 /** Amount of tokens added per refill */
+};
+
+// For testing purposes
+function print_bucket() {
+  console.log(`
+  Bucket status:
+  - capacity : ${bucket.capacity}
+  - amount : ${bucket.amount}
+  - interval : ${bucket.interval}
+  - last_fill : ${bucket.last_fill}
+  - refill_amt : ${bucket.refill_amt}
+  `);
+  return;
 }
 
-app.get('/', (req, res) => {
-  res.send('Welcome to EQ Works 😎')
-})
+function fill_bucket() {
+  let curr_time = new Date().getTime();
+  let num_refills = Math.floor(
+    (curr_time - bucket.last_fill) / bucket.interval
+  );
+  if (num_refills > 0) {
+    bucket.amount += bucket.refill_amt * num_refills;
+    /** If tokens are at capacity, don't fill anymore */
+    bucket.amount = Math.min(bucket.amount, bucket.capacity);
+    bucket.last_fill = curr_time;
+    return true;
+  }
+  return false;
+}
 
-app.get('/events/hourly', (req, res, next) => {
-  req.sqlQuery = `
+const queryHandler = (req, res, next) => {
+  let filled = fill_bucket(); // checks for refill interval
+  if (bucket.amount > 0) {
+    pool
+      .query(req.sqlQuery)
+      .then(r => {
+        return res.json(r.rows || []);
+      })
+      .catch(next);
+    bucket.amount -= 1;
+  } else {
+    console.log(`
+      ERROR: Too many requests. Please try again later.
+    `);
+  }
+  // print_bucket(); // For testing purposes
+};
+
+app.get("/", (req, res) => {
+  console.log("Hello World!");
+  res.send("Welcome to EQ Works 😎");
+});
+
+app.get(
+  "/events/hourly",
+  (req, res, next) => {
+    req.sqlQuery = `
     SELECT date, hour, events
     FROM public.hourly_events
     ORDER BY date, hour
     LIMIT 168;
-  `
-  return next()
-}, queryHandler)
+  `;
+    return next();
+  },
+  queryHandler
+);
 
-app.get('/events/daily', (req, res, next) => {
-  req.sqlQuery = `
+app.get(
+  "/events/daily",
+  (req, res, next) => {
+    req.sqlQuery = `
     SELECT date, SUM(events) AS events
     FROM public.hourly_events
     GROUP BY date
     ORDER BY date
     LIMIT 7;
-  `
-  return next()
-}, queryHandler)
+  `;
+    return next();
+  },
+  queryHandler
+);
 
-app.get('/stats/hourly', (req, res, next) => {
-  req.sqlQuery = `
+app.get(
+  "/stats/hourly",
+  (req, res, next) => {
+    req.sqlQuery = `
     SELECT date, hour, impressions, clicks, revenue
     FROM public.hourly_stats
     ORDER BY date, hour
     LIMIT 168;
-  `
-  return next()
-}, queryHandler)
+  `;
+    return next();
+  },
+  queryHandler
+);
 
-app.get('/stats/daily', (req, res, next) => {
-  req.sqlQuery = `
+app.get(
+  "/stats/daily",
+  (req, res, next) => {
+    req.sqlQuery = `
     SELECT date,
         SUM(impressions) AS impressions,
         SUM(clicks) AS clicks,
@@ -57,33 +127,39 @@ app.get('/stats/daily', (req, res, next) => {
     GROUP BY date
     ORDER BY date
     LIMIT 7;
-  `
-  return next()
-}, queryHandler)
+  `;
+    return next();
+  },
+  queryHandler
+);
 
-app.get('/poi', (req, res, next) => {
-  req.sqlQuery = `
+app.get(
+  "/poi",
+  (req, res, next) => {
+    req.sqlQuery = `
     SELECT *
     FROM public.poi;
-  `
-  return next()
-}, queryHandler)
+  `;
+    return next();
+  },
+  queryHandler
+);
 
-app.listen(process.env.PORT || 5555, (err) => {
+app.listen(process.env.PORT || 5555, err => {
   if (err) {
-    console.error(err)
-    process.exit(1)
+    console.error(err);
+    process.exit(1);
   } else {
-    console.log(`Running on ${process.env.PORT || 5555}`)
+    console.log(`Running on ${process.env.PORT || 5555}`);
   }
-})
+});
 
 // last resorts
-process.on('uncaughtException', (err) => {
-  console.log(`Caught exception: ${err}`)
-  process.exit(1)
-})
-process.on('unhandledRejection', (reason, p) => {
-  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason)
-  process.exit(1)
-})
+process.on("uncaughtException", err => {
+  console.log(`Caught exception: ${err}`);
+  process.exit(1);
+});
+process.on("unhandledRejection", (reason, p) => {
+  console.log("Unhandled Rejection at: Promise", p, "reason:", reason);
+  process.exit(1);
+});
